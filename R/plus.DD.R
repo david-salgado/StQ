b5818258fee8a1bd35256c5cf1dba680432ed88c
elisa-esteban <elisa.esteban.segurado@gmail.com>
2016-01-20 14:05:43 +0100

    Updating function to adapt to the new DD-class

--- R/plus.DD.R
+++ R/plus.DD.R
@@ -13,16 +13,31 @@
 #'
 #' @examples
 #' library(data.table)
+#' VNC1List <- list(data.table(IDQual = c('Norden', '', ''),
+#'                             NonIDQual = c('', 'CCAA', ''),
+#'                             IDDD = c('', '', 'CifraNeg'),
+#'                             Norden = c('', '', ''),
+#'                             CCAA = c('', '', ''),
+#'                             Unit1 = c('', '', '')))
+#' names(VNC1List) <- seq(along = VNC1List)
+#' VNC1 <- new(Class = 'VarNameCorresp', VarNameCorresp = VNC1List)
 #' DD1 <- new(Class = 'DD',
 #'            Data = data.table(Variable = c('NOrden', 'CCAA', 'CifraNeg'),
 #'                              Sort = c('IDQual', 'NonIDQual', 'IDDD'),
-#'                              Class = c('character', 'character', 'numeric'),
-#'                              Qual1 = c('', '', 'NOrden')))
+#'                              Class = c('character', 'character', 'numeric'), 
+#'                              Qual1 = c('', '', 'NOrden')),
+#'            VarNameCorresp = VNC1)
+#' VNC2List <- list(data.table(IDQual = c('Norden',''), NonIDQual = c('', ''),
+#'                             IDDD = c('', 'logCifraNeg'),
+#'                             Norden = c('', ''),
+#'                             Unit1 = c('', '')))
+#' names(VNC2List) <- seq(along = VNC2List)
+#' VNC2 <- new(Class = 'VarNameCorresp', VarNameCorresp = VNC2List)
 #' DD2 <- new(Class = 'DD',
-#'            Data = data.table(Variable = c('logCifraNeg'),
-#'                              Sort = c('IDDD'),
-#'                              Class = c('numeric'),
-#'                              Qual1 = c('NOrden')))
+#'            Data = data.table(Variable = c('Norden', 'logCifraNeg'),
+#'                              Sort = c('IDQual', 'IDDD'),
+#'                              Class = c('character', 'numeric'),
+#'                              Qual1 = c('', 'NOrden')), VarNameCorresp = VNC2)
 #' DD1 + DD2
 #'
 #' @include DD-class.R getData.R
@@ -61,11 +76,64 @@ setMethod(
             }
         }
 
-        output <- rbindlist(list(Data1, Data2))
-        setkeyv(output, names(output))
-        output <- output[!duplicated(output)]
-        output <- new(Class = 'DD', Data = output)
-        return(output)
+        DataPlus <- rbindlist(list(Data1, Data2))
+        setkeyv(DataPlus, names(DataPlus))
+        DataPlus <- DataPlus[!duplicated(DataPlus)]
+        
+        namesVNC1 <- names(e1@VarNameCorresp@VarNameCorresp)
+        namesVNC2 <- names(e2@VarNameCorresp@VarNameCorresp)
+        CommonSheets <- as.list(intersect(namesVNC1, namesVNC2))
+        
+        VNCListPlus <- lapply(CommonSheets, function(SheetName){
+            
+            SheetName1 <- e1@VarNameCorresp@VarNameCorresp
+            if (SheetName %in% names(SheetName1)){
+                
+                SheetName1 <- SheetName1[[names(SheetName1) == SheetName]]
+                SheetName2 <- e2@VarNameCorresp@VarNameCorresp
+                SheetName2 <- SheetName2[[names(SheetName2) == SheetName]]
+                
+                colSheet1 <- names(SheetName1)
+                colSheet2 <- names(SheetName2)
+                CommonSheetCol <- intersect(colSheet1, colSheet2)
+                colSheet1NotSheet2 <- setdiff(colSheet1, colSheet2)
+                if (length(colSheet1NotSheet2) > 0) {
+                    
+                    for (NewCol in colSheet1NotSheet2){
+                        
+                        SheetName2[, NewCol := '', with = F]
+                        
+                    }
+                }
+                colSheet2NotSheet1 <- setdiff(colSheet2, colSheet1)
+                if (length(colSheet2NotSheet1) > 0) {
+                    
+                    for (NewCol in colSheet2NotSheet1){
+                        
+                        SheetName1[, NewCol := '', with = F]
+                        
+                    }
+                }
+                output <- rbindlist(list(SheetName1, SheetName2))
+                output <- output[!duplicated(output)]
+                setkeyv(output, names(output))
+                setorderv(output, names(output), rep(-1, length(output)))
+                columns <- names(output)
+                ColIDQual <- output[[columns[1]]]
+                ColIDQual <- ColIDQual[ColIDQual!=""]
+                ColIDNonQual <- output[[columns[2]]]
+                ColIDNonQual <- ColIDNonQual[ColIDNonQual!=""]
+                colAdd <- c(ColIDQual, ColIDNonQual)
+                colorder <- c(columns[1:3], colAdd)
+                colorder <- c(colorder, setdiff(columns, colorder))
+                setcolorder(output, colorder)
+            }
+        })
+        
+        VNCPlus <- new(Class = 'VarNameCorresp', VarNameCorresp = VNCListPlus)
+        plusDD <- new(Class = 'DD', Data = DataPlus, VarNameCorresp = VNCPlus)
+        
+        return(plusDD)
 
     }
 )
