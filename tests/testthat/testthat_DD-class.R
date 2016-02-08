9170e1a104befb2d64ecac890552a91a673b0330
david-salgado <david.salgado.fernandez@gmail.com>
2016-02-08 13:52:45 +0100

    Including tests (round 1)

---
+++ tests/testthat/testthat_DD-class.R
@@ -0,0 +1,15 @@
+library(StQ)
+context("Class DD")
+
+test_that("initializer returns the correct empty DD class object", {
+    EmptyDD <- new(Class = 'DD')
+    expect_that(slotNames(EmptyDD), is_identical_to(c('VarNameCorresp', 
+                                                      'MicroData',
+                                                      'Aggregates',
+                                                      'AggWeights',
+                                                      'Other'))
+    )
+    expect_that(slot(EmptyDD, 'VarNameCorresp'), 
+                is_identical_to(new(Class = 'VarNameCorresp'))
+    )
+})
