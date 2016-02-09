b023e0fc202dd7e4bf75240622473dbf942dde74
elisa-esteban <elisa.esteban.segurado@gmail.com>
2016-02-09 09:31:06 +0100

    Updating man pages and defining new getters for class DD

---
+++ R/getAggWeights.R
@@ -0,0 +1,31 @@
+#' @title Return slot AggWeights from a DD object
+#'
+#' @description \code{getAggWeights} returns slot \code{AggWeights} of the input
+#' \linkS4class{DD} object.
+#' 
+#' @param object a DD Object whose slot \code{AggWeights} is queried.
+#'
+#' @return \linkS4class{data.table} with data from slot \code{AggWeights} of the
+#' input \linkS4class{DD} object.
+#'
+#' @examples
+#' # An example with data created previosly:
+#' data(ExampleDD)
+#' getAggWeights(ExampleDD)
+#' 
+#' @export
+setGeneric("getAggWeights", function(object){standardGeneric("getAggWeights")})
+#' @rdname getAggWeights
+#' 
+#' @include DD-class.R
+#' 
+#' @export
+setMethod(
+    f = "getAggWeights",
+    signature = c("DD"),
+    function(object){
+        
+        out <- object@AggWeights
+        return(out)
+    }
+)
---
+++ R/getOtherDD.R
@@ -0,0 +1,31 @@
+#' @title Return slot Other from a DD object
+#'
+#' @description \code{getOtherDD} returns slot \code{Other} of the input
+#' \linkS4class{DD} object.
+#' 
+#' @param object a DD Object whose slot \code{Other} is queried.
+#'
+#' @return \linkS4class{data.table} with data from slot \code{Other} of the
+#' input \linkS4class{DD} object.
+#'
+#' @examples
+#' # An example with data created previosly:
+#' data(ExampleDD)
+#' getOtherDD(ExampleDD)
+#' 
+#' @export
+setGeneric("getOtherDD", function(object){standardGeneric("getOtherDD")})
+#' @rdname getAggWeights
+#' 
+#' @include DD-class.R
+#' 
+#' @export
+setMethod(
+    f = "getOtherDD",
+    signature = c("DD"),
+    function(object){
+        
+        out <- object@Other
+        return(out)
+    }
+)
