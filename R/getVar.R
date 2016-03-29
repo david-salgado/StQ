c95423ac83061437408a5aa55b3d846162fd1ed7
soledad-saldana <soledad.saldana.diaz@gmail.com>
2016-03-29 12:19:46 +0200

    Correct getVar; update to version 0.3.2.9000

--- R/getVar.R
+++ R/getVar.R
@@ -15,12 +15,14 @@
 #'
 #' @examples
 #' library(data.table)
-#' Units <- data.table(NOrden = 1:5)
+#' Units <- data.table(NOrden = paste0('0000000000', 1:9))
 #' getVar(ExampleQ, 'IASSCifraNeg', Units)
 #' getVar(ExampleQ, 'IASSEmpleo', Units)
 #' getVar(ExampleQ, 'IASSEmpleo_0', Units)
 #' getVar(ExampleQ, 'IASSEmpleo_1', Units)
 #' getVar(ExampleQ, 'IASSEmpleo_1_1', Units)
+#'
+#' @import data.table
 #'
 #' @export
 setGeneric("getVar", function(object, VarName, Units = getUnits(object)){standardGeneric("getVar")})
@@ -29,8 +31,8 @@ setGeneric("getVar", function(object, VarName, Units = getUnits(object)){standar
 #'
 #' @include StQ-class.R getData.R getDD.R VarNamesToDD.R
 #'
-#' @import data.table
-#'
+#' @import data.table RepoTime
+#' 
 #' @export
 setMethod(
     f = "getVar",
@@ -43,22 +45,43 @@ setMethod(
 
         }
         Data <- getData(object)
-        setkeyv(Data, names(Units))
-        Data <- Data[Units]
+        #setkeyv(Data, names(Units))
+        Data <- merge(Data, Units, by = names(Units), all.y = TRUE)
         DD <- getDD(object)
+        
         Var <- VarNamesToDD(VarName, DD)
+        for (col in names(Var)){
+            
+            Data[, col := ifelse(is.na(get(col)), Var[[col]], get(col)), with = FALSE]
+        }
+
         Data <- merge(Data, Var, by = names(Var))
         Data <- Data[, c(names(Units), 'Value'), with = F]
-        if (dim(Data)[1] == 0) return(Data)
-        Data[, Unit := '']
-        for (col in setdiff(names(Data), c('Unit', 'Value'))){
-            Data[, Unit := ifelse(Unit == '', get(col), paste0(Unit, '_', get(col)))]
+
+        if (dim(Data)[1] == 0) {
+            
+            newUnits <- copy(Units)
+            newUnits[, Unit := '']
+            for (col in names(Units)){
+                newUnits[, Unit := ifelse(Unit == '', get(col), paste0(Unit, '_', get(col)))]
+            }
+            output <- matrix(rep(NA, dim(newUnits)[1]), ncol = 1, dimnames = list(newUnits[['Unit']], VarName))
+            
+        } else {
+        
+            output <- Data[['Value']]
+            Data[, Unit := '']
+            for (col in setdiff(names(Data), c('Unit', 'Value'))){
+                Data[, Unit := ifelse(Unit == '', get(col), paste0(Unit, '_', get(col)))]
+            }
+            VarClass <- getData(DD)[Variable == ExtractNames(VarName)][['Class']]
+            output <- as(output, VarClass)
+            output <- matrix(output, ncol = 1, dimnames = list(Data[['Unit']], VarName))
+            
         }
-        output <- Data[['Value']]
-        VarClass <- getData(DD)[Variable == ExtractNames(VarName)][['Class']]
-        output <- as(output, VarClass)
-        output <- matrix(output, ncol = 1, dimnames = list(Data[['Unit']], VarName))
+
         return(output)
+        
     }
 )
 
@@ -85,7 +108,9 @@ setMethod(
             out <- getVar(StQ, VarName = VarName, Units = Units)
             return(out)
         })
-        output <- unlist(output)
+
+        output <- Reduce(cbind, output)
+        colnames(output) <- getRepo(object@Periods)
         return(output)
     }
 )
