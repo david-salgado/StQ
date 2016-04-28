622d3e7c58b802ecd73159ec02ad4828dcf9bd41
david-salgado <david.salgado.fernandez@gmail.com>
2016-04-28 13:16:07 +0200

    Curate examples

--- R/Datadt-class.R
+++ R/Datadt-class.R
@@ -17,10 +17,14 @@
 #'
 #' @examples
 #' new(Class = 'Datadt', 
-#'     data.table(ID = c('001', '001', '001', '001'), 
-#'                IsNatMarket = c('0', '1', '', ''),
-#'                IDDD = c('Turnover', 'Turnover', 'Province', 'NACE09'),
-#'                Value = c('625000', '23154', '04', '0512')))
+#'     data.table(ID = c('001', '001', '001', '001', 
+#'                       '002', '002', '002', '002'), 
+#'                IsNatMarket = c('0', '1', '', '',
+#'                                '0', '1', '', ''),
+#'                IDDD = c('Turnover', 'Turnover', 'Province', 'NACE09',
+#'                         'Turnover', 'Turnover', 'Province', 'NACE09'),
+#'                Value = c('625000', '23154', '04', '0512',
+#'                          '25345', '1224', '01', '0601')))
 #' @import data.table
 #'
 #' @export
