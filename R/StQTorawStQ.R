199ebd77255a0e84fa1cc3cf0726a77e13d5fad3
elisa-esteban <elisa.esteban.segurado@gmail.com>
2016-07-04 14:26:44 +0200

    Solving conflicts.

--- R/StQTorawStQ.R
+++ R/StQTorawStQ.R
@@ -29,29 +29,10 @@ setMethod(
         
         QDD <- getDD(Q)
         
-<<<<<<< HEAD
-        QData <- getData(Q)
-        #nsep <- length(names(QData))
-        QData <- DTToKey(QData)
-        value <- unlist(lapply(strsplit(QData, '@@'), function(x){x[length(x)]}))
-        key <-  lapply(strsplit(QData, '@@'), function(x){x[1:(length(x) - 1)]})
-        #value <- unlist(lapply(StrSplit(QData@.Data, nsep, '@@'), function(y){y[length(y)]}))
-        #key <-  lapply(StrSplit(QData@.Data, nsep, '@@'), function(y){y[1:(length(y) - 1)]})
-        key <- lapply(key, paste0, '@@')
-        key <- unlist(lapply(key, function(x){Reduce(paste0, x)}))
-||||||| merged common ancestors
-        QData <- getData(Q)
-        QData <- DTToKey(QData)
-        value <- unlist(lapply(strsplit(QData, '@@'), function(x){x[length(x)]}))
-        key <-  lapply(strsplit(QData, '@@'), function(x){x[1:(length(x) - 1)]})
-        key <- lapply(key, paste0, '@@')
-        key <- unlist(lapply(key, function(x){Reduce(paste0, x)}))
-=======
         QData <- DatadtToDT(getData(Q))
         Value <- QData[['Value']]
         QData[, Value := NULL]
         key <- DTToKey(QData)
->>>>>>> ff277f6cdd5fddd61ed2d53b488ca2b0d15d8b1f
         key <- new(Class = 'rawKey', key)    
         rawDatadt <- new(Class = 'rawDatadt', data.table(Key = key, Value = Value))
         rawQ <- new(Class = 'rawStQ', Data = rawDatadt, DD = QDD)
