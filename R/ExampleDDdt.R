c794d64bd6615aa4b55eb1d579c2219cad21e31f
elisa-esteban <elisa.esteban.segurado@gmail.com>
2016-07-11 14:39:50 +0200

    Updating help in examples.

--- R/ExampleDDdt.R
+++ R/ExampleDDdt.R
@@ -2,15 +2,15 @@
 #'
 #' @description \code{ExampleDDdt} is an object of class \linkS4class{DDdt}.
 #'
-#' This class is a subclass of \linkS4class{data.table} with columns 
-#' \code{Variable}, \code{Sort}, \code{Class}, \code{Qual}\emph{1}-\emph{j}, and
-#' \code{ValueRegExp}.
+#' This class is a subclass of \linkS4class{data.table} with columns \code{Variable}, \code{Sort},
+#' \code{Class}, \code{Length},\code{Qual}\emph{1}-\emph{j}, and \code{ValueRegExp}.
 #'
 #' The meaning of these columns are:
 #' \itemize{
 #'   \item \code{Variable}: statistical variable name.
 #'   \item \code{Sort}: semantic sort of the variable (IDQual, NonIDQual, IDDD).
 #'   \item \code{Class}: class of the variable (\code{numeric}, \code{character},...).
+#'   \item \code{Length}: highest length for each variable.
 #'   \item \code{Qual1} to \code{Qual3}: 1st to 3rd variable qualifier.
 #'   \item \code{ValueRegExp}: regexp for the variable values.
 #' }
@@ -21,5 +21,5 @@
 #'
 #' @usage data(ExampleDDdt)
 #'
-#' @format Object of class \linkS4class{DDdt} with 14 rows and 7 columns.
+#' @format Object of class \linkS4class{DDdt} with 14 rows and 8 columns.
 NULL
--- R/ExampleDatadt.R
+++ R/ExampleDatadt.R
@@ -1,9 +1,8 @@
 #' @title Object of class \linkS4class{Datadt}
 #'
-#' @description \code{ExampleDatadt} is an object of class \linkS4class{Datadt} 
-#' with key-value pair structure for the included severely perturbed anonymised 
-#' data from a real statistical operation. The class \linkS4class{Datadt} is a
-#' subclass of \linkS4class{data.table}.
+#' @description \code{ExampleDatadt} is an object of class \linkS4class{Datadt} with key-value pair
+#' structure for the included severely perturbed anonymised data from a real statistical operation.
+#' The class \linkS4class{Datadt} is a subclass of \linkS4class{data.table}.
 #'
 #' The columns this object are:
 #' \itemize{
@@ -21,5 +20,5 @@
 #'
 #' @usage data(ExampleDatadt)
 #'
-#' @format Object of class \linkS4class{Datadt} with 354456 rows and 5 columns.
+#' @format Object of class \linkS4class{Datadt} with 383994 rows and 5 columns.
 NULL
