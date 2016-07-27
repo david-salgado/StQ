9cc0753b84caffd9039dbc1e9281e584639e2c67
david-salgado <david.salgado.fernandez@gmail.com>
2016-07-27 18:03:11 +0200

    Modify DDdt-class

--- R/DDdt-class.R
+++ R/DDdt-class.R
@@ -41,10 +41,10 @@ setClass(Class = "DDdt",
              NCol <- dim(object)[2]
              
              if (NCol < 6) {
-                 stop(paste0('[Validity DDdt] The object must be a data.table with 
-                                at least five columns named "Variable", "Sort", "Class", 
-                                "QualOrder", "Qual1" and "ValueRegExp"'))   
-             } 
+                 
+                 stop(paste0('[Validity DDdt] The object must be a data.table with at least five columns named "Variable", "Sort", "Class", "QualOrder", "Qual1" and "ValueRegExp"'))   
+             
+            } 
              
              
              Data <- slot(object, ".Data")
